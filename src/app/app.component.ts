701ca23e29c7d5bf809455176cbddecec10c74a1
unknown <saurabh955920@gmail.com>
2026-05-30 15:30:50 +0530

    App divided into reusable components lec - 103

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import {HeaderComponent} from './header/header.component';
+import { TodoComponent } from './todo/todo.component';
+import { ServerDashboardComponent } from './server-dashboard/server-dashboard.component';
+import { TrafficDashboardComponent } from './traffic-dashboard/traffic-dashboard.component';
+import { DashboardItemComponent } from './dashboard/dashboard-item/dashboard-item.component';
+@Component({
+
+  selector: 'app-root',
+  standalone: true,
+  imports: [HeaderComponent, TodoComponent,ServerDashboardComponent,TrafficDashboardComponent,DashboardItemComponent],
+  templateUrl: './app.component.html',
+})
+export class AppComponent {
+
+}
---
+++ src/app/server-dashboard/server-dashboard.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-server-dashboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './server-dashboard.component.html',
+  styleUrl: './server-dashboard.component.css'
+})
+export class ServerDashboardComponent {
+  currentStatus = 'online';
+
+}
