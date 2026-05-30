af320eb56f585beb4aa279a17e375d0b4cfe962e
unknown <saurabh955920@gmail.com>
2026-05-30 17:03:21 +0530

    extended button functionality using attribute selector

--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -1,7 +1,25 @@
 import { Component, input } from '@angular/core';
 
 @Component({
-  selector: 'app-button',
+  // selector: 'app-button',
+  selector:'button[appButton]',
+  /*
+  This is -> "Attribute selector"
+  Attribute selectors let you use a component on an existing HTML element instead of creating a new custom tag.
+
+  When to use it?
+Use attribute selectors when:
+You only want to add behavior/styles to an existing element.
+You want cleaner DOM.
+The component is basically a styled version of a native element (button, input, textarea, etc.).
+
+Simple rule
+Need a completely new UI block?
+→ Use normal selector (app-user)
+Need a styled/enhanced HTML element?
+→ Use attribute selector (button[appButton])
+
+*/
   standalone: true,
   imports: [],
   templateUrl: './button.component.html',
