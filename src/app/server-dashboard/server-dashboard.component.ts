403ec98bd4108c571f97f2f46f303b2a906ef733
unknown <saurabh955920@gmail.com>
2026-05-31 01:32:55 +0530

    introducing the component lifecycle:ngOnInit(){}

--- src/app/server-dashboard/server-dashboard.component.ts
+++ src/app/server-dashboard/server-dashboard.component.ts
@@ -6,24 +6,26 @@ import { Component } from '@angular/core';
   imports: [],
   templateUrl: './server-dashboard.component.html',
   styleUrl: './server-dashboard.component.css',
-
 })
 export class ServerDashboardComponent {
+  currentStatus: 'online' | 'offline' | 'unknown' = 'online';
 
-  currentStatus: 'online'|'offline'|'unknown'  = 'online';
+  constructor() {}
 
-  constructor(){
-    setInterval(()=>{
+  ngOnInit() {
+    setInterval(() => {
       const rnd = Math.random();
-      if(rnd < 0.5){
+      if (rnd < 0.5) {
         this.currentStatus = 'online';
-      }
-      else if(rnd < 0.9){
+      } else if (rnd < 0.9) {
         this.currentStatus = 'offline';
-      }else{
-        this.currentStatus="unknown"
+      } else {
+        this.currentStatus = 'unknown';
       }
-    },3000);
+    }, 3000);
   }
-
+  /*
+  it is advised to keep the constructor lean, so constructor is used when we want to pereform a task during creation  of a class
+  whereas ngOnInit is used when "change detection" happens so it "rund after the angular has initialized all the component's input"
+  */
 }
