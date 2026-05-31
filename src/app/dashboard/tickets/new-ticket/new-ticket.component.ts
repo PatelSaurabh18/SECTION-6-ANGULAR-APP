151379824d7980d1457681895b23a929428ba3a9
unknown <saurabh955920@gmail.com>
2026-05-31 14:49:56 +0530

    Handling form submission:with template variables, alternative to ngModel & 2way binding

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from '../../../shared/control/control.component';
+import { log } from 'node:console';
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ButtonComponent,ControlComponent],
+  imports: [ButtonComponent,ControlComponent,FormsModule],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
-
+  onSubmit(titleElement :HTMLInputElement){
+    // console.dir(titleElement);
+    // console.log("SUBMITTED");
+    const title = titleElement.value;
+    console.log(title);
+  }
 }
